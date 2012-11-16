85016007af82cc559bddb165dc3c49d72dcb07ed
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2012-11-16 13:28:08 +0000

    simple supervisor to manage ets tables.

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 ets_manager
 ===========
 
-Separate management of ets tables from processes to prevent lost data.
+Separate management of ets tables to prevent lost data.
+
+## Description
+
+This library will allow tables to be created that will
+have it's 'heir' as the manger process. It will then assign
+the table to the calling process. If the process dies
+unexpectedly the table returns to the manager process.
+
+Add the library to your rebar config
+
+```
+{
+  deps,
+  [
+   {ets_manager, "0.1.*", {git, "git://github.com/dipthegeezer/ets_manager.git", "HEAD"}},
+   ....
+  ]
+}.
+
+```
+Then just use rebar to get the deps and use in your code.
+
+```
+./rebar get-deps
+
+```
+
+## Usage
+
+### API
+
+```
+application:start(ets_manager).
+P = spawn(
+        fun() ->
+            Name = foo,
+            {ok, Name} = ets_manager:give_me(Name),
+            receive
+                {'ETS-TRANSFER', Tid, _Pid, new_table} -> some_function(Tid)
+            end
+         end.
+    ),
+%%kill and see if reassigns.
+exit(P, zeds_dead_baby),
+%%Reissue the table
+Z = spawn(
+        fun() ->
+           Name = foo,
+           {ok, Name} = ets_manager:give_me(Name),
+           receive
+               {'ETS-TRANSFER', Tid, _Pid, reissued} -> some_function(Tid)
+           end
+        end.
+    ).
+
+```
---
+++ src/ets_manager.erl
@@ -0,0 +1,149 @@
+-module(ets_manager).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%%TODO Avoid using named_tables allow for flexibilty. Log
+%%TODO Log reassignments.
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0, give_me/1]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% Record Definitions
+%% ------------------------------------------------------------------
+
+%% @type state() = Record :: #state{ opts = term()
+%%                                 }.
+
+-record(state, {opts}).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+give_me(Name) ->
+    gen_server:call(?MODULE, {give_me, Name}).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+%%----------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok, State} |
+%%          {ok, State, Timeout} |
+%%          ignore |
+%%          {stop, Reason}
+%%----------------------------------------------------------------------
+init([]) ->
+    Opts = [set, named_table, protected, {keypos,1}, {heir,self(),[]},
+            {write_concurrency,false}, {read_concurrency,false}],
+    {ok, #state{opts=Opts}}.
+
+handle_call({give_me, Name}, {Pid, _Tag}, State) ->
+    Return = give_me(Name, Pid, State),
+    {reply, Return, State};
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+%% receive message from dead process. Note returned table.
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+%% @spec (Name, Pid, State) -> { ok, Tab } | {error, Reason }
+%%        Name = atom()
+%%        State = state()
+%%        Pid = pid()
+%%        Tab = ets:tab()
+%%        Reason = atom()
+%% @doc Create or return an ets table for use. It will make the ets_manager
+%% a heir on the table so that on failure it is returned to it.
+give_me(Name, Pid, State) ->
+    Me = self(),
+    case ets:info(Name) of
+        undefined -> Tid = ets:new(Name, State#state.opts),
+                     case ets:give_away(Tid, Pid, new_table) of
+                         true -> {ok, Tid};
+                         false -> {error, cant_give_away}
+                     end;
+        _Found -> case ets:info(Name, owner) of
+                      Pid -> {error, already_own_table};
+                      Me  -> case ets:give_away(Name, Pid, reissued) of
+                                 true -> {ok, Name}; %% Name =:= Tid
+                                 false -> {error, cant_give_away}
+                             end
+                  end
+    end.
+
+%% ===================================================================
+%% EUnit tests
+%% ===================================================================
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+give_me_test() ->
+    Name = foo,
+    Opts = [set, named_table, protected, {keypos,1}, {heir,self(),[]},
+            {write_concurrency,false}, {read_concurrency,false}],
+    State = #state{opts=Opts},
+    P = spawn( new_check(Name) ),
+    {ok,Name} = give_me(Name, P, State),
+    %%kill and see if reassigns.
+    exit(P, zeds_dead_baby),
+    return_check(Name, P),
+    Z = spawn( reissue_check(Name) ),
+    {ok,Name} = give_me(Name, Z, State).
+
+new_check(Name) ->
+    fun() ->
+        receive
+            {'ETS-TRANSFER', Tid, _Pid, new_table} ->
+                ?assertEqual( Name, Tid );
+            Error -> ?assertEqual(ok,Error)
+        end
+    end.
+
+return_check(Name, Pid) ->
+    receive
+        {'ETS-TRANSFER', Tid, Pid, []} ->
+            ?assertEqual( Name, Tid );
+        Error -> ?assertEqual(ok, Error)
+    end.
+
+reissue_check(Name) ->
+    fun() ->
+        receive
+            {'ETS-TRANSFER', Tid, _Pid, reissued} ->
+                ?assertEqual( Name, Tid );
+            Error -> ?assertEqual(ok,Error)
+        end
+    end.
+
+-endif.
